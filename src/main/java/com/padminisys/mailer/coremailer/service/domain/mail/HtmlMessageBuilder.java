d5c0278070fdea6337c1b506b4723f66c58fb09c
ramanujd <ramanuj@padminisys.com>
2022-02-04 23:35:06 +0530

    Removed unwanted files

--- src/main/java/com/padminisys/mailer/coremailer/service/domain/mail/HtmlMessageBuilder.java
+++ src/main/java/com/padminisys/mailer/coremailer/service/domain/mail/HtmlMessageBuilder.java
@@ -5,7 +5,6 @@ import com.padminisys.mailer.coremailer.service.model.Mailer;
 import io.micrometer.core.instrument.util.IOUtils;
 import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Tag;
 import org.springframework.core.io.Resource;
@@ -17,7 +16,6 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -32,17 +30,20 @@ public class HtmlMessageBuilder {
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
         helper.setFrom(mailer.getClient().getMailId());
         helper.setReplyTo(mailer.getClient().getReplyToMailId());
-        helper.setSubject("Vikhyaath Developers Launched - RRR-DTCP");
+
         Resource resource = resourceLoader.getResource("classpath:" + resourceRootPath + "/mail.html");
         MailTransaction mailTransaction = mailTransactionBuilder.build(mailer);
         String html = IOUtils.toString(resource.getInputStream(), StandardCharsets.UTF_8);
-        Document document = Jsoup.parse(html);
+
+        Element divWrapper = new Element(Tag.valueOf("div"), "")
+                .attr("style", "text-align: center");
         Element link = new Element(Tag.valueOf("a"), "")
                 .text("Click here to unsubscribe from our mailing list.")
                 .attr("href", "https://padmini-mailer.herokuapp.com/contact/unsubscribe?mailTransactionId=" + mailTransaction.getId())
                 .attr("target", "_blank");
-        Objects.requireNonNull(document.getElementById("unsubscribe")).appendChild(link);
-        helper.setText(document.html(), true);
+        divWrapper.appendChild(link);
+        helper.setSubject(Jsoup.parse(html).title());
+        helper.setText(html + divWrapper.outerHtml(), true);
         mailer.setMimeMessageHelper(helper);
         mailer.setMimeMessage(mimeMessage);
         mailer.setMailTransaction(mailTransaction);
